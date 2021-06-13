3c116df119e7af1629915d7cb13d28fc50a42104
m0h1572 <59693527+m0h1572@users.noreply.github.com>
2021-06-13 17:44:22 +0000

    command mesaji yaptim bot geri cevap vermesi

---
+++ command.js
@@ -0,0 +1,23 @@
+const { prefix } = require('./config.json')
+
+
+
+//burda mesaj secilme ve prefixi tutma var command vermek icin
+module.exports = (client, aliases, callback) => {
+  if (typeof aliases === 'string') {
+    aliases = [aliases
+    ]
+  }
+  client.on('message', message => {
+    const { content
+    } = message;
+    aliases.forEach(alias => {
+      const command = `${prefix}${alias}`
+
+      if (content.startsWith(`${command} `) || content === command) {
+        console.log(`Running the command ${command}`)
+        callback(message)
+      }
+    })
+  })
+}
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const Discord = require('discord.js'); 
 const client = new Discord.Client();
+
 const config = require ('./config.json')
+const command = require('./command')
 
 client.on('ready', () => {
-	console.log('The client is ready!');
+	console.log('Bot suan aktif!');
+
+  command(client,['ping', 'test'], message => {
+    message.channel.send('pong!')
+  })
 });
 
 client.login(config.token)
